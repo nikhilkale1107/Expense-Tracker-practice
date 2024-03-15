94a01880db031c6edbc9c5ae0abc733f8087b764
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-15 16:39:58 +0530

    Authentication added

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -4,12 +4,14 @@ const router = express.Router();
 
 const exprenseController = require("../controllers/expense");
 
+const Authenticate = require("../middleware/auth");
 
 
 
-router.post("/", exprenseController.postExpense);
-router.get("/", exprenseController.getExpenses);
-router.delete("/:id", exprenseController.deleteExpense);
+
+router.post("/",Authenticate, exprenseController.postExpense);
+router.get("/",Authenticate, exprenseController.getExpenses);
+router.delete("/:id",Authenticate, exprenseController.deleteExpense);
 
 
 module.exports = router;
