7dff6c90dd72dff4508a98caf4f8ab48294fb3a7
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-15 10:46:34 +0530

    first commit

---
+++ frontend/Signup/signup.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", register);
+  });
+  
+  function register() {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    axios
+      .post("http://localhost:4000/user/register", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
