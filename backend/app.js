18282a6283c2e4c06e593ed18f31ed482b31d190
Nikhil Kale <nikhilkale1107@gmail.com>
2024-03-16 15:22:24 +0530

    Payment Gateway Added

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,10 @@ const app = express();
 const sequelize = require('./utils/database');
 const userRouter = require('./routes/user');
 const expenseRouter = require('./routes/expense');
+const paymentRouter = require('./routes/payment');
 const User = require("./models/user");
 const Expense = require("./models/expense");
+const Order = require("./models/order");
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,12 +14,15 @@ const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.use("/user", userRouter);
 app.use("/expense", expenseRouter);
+app.use("/payment", paymentRouter);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);
+
+User.hasMany(Order);
+Order.belongsTo(User);
 
 sequelize.sync().then(() => {
     app.listen(4000, () => {
--- frontend/Home/index.js
+++ frontend/Home/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const expenseForm = document.getElementById("expenseForm");
     const expensesList = document.getElementById("expenses");
+    const token = JSON.parse(localStorage.getItem("token")).token;
 
     expenseForm.addEventListener("submit", function (event) {
       event.preventDefault();
@@ -44,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(function (response) {
           let data = response.data;
 
-          data.expenses.forEach(function (expense, index) {
+          data.forEach(function (expense, index) {
             const li = document.createElement("li");
             li.className = "list-group-item";
             const deleteButton = document.createElement("button");
@@ -63,4 +64,60 @@ document.addEventListener("DOMContentLoaded", function () {
           console.log(error);
         });
     }
+    displayExpenses()
   });
+
+  document.getElementById("buy-button").onclick = async function (e) {
+    e.preventDefault();
+    const token = JSON.parse(localStorage.getItem("token")).token;
+    const result = await axios.post(
+      "http://localhost:4000/payment/createOrder",
+      null,
+      {
+        headers: { Authorization: token },
+      }
+    );
+    var options = {
+      key: result.data.key_id,
+      order_id: result.data.order.id,
+      handler: async function (response) {
+        await axios.post(
+          "http://localhost:4000/payment/updateOrder",
+          {
+            order_id: result.data.order.id,
+            payment_id: response.razorpay_payment_id,
+            status: "SUCCESS",
+          },
+          {
+            headers: { Authorization: token },
+          }
+        );
+        showPremiumUser();
+        localStorage.setItem(
+          "token",
+          JSON.stringify({
+            token: JSON.parse(localStorage.getItem("token")).token,
+            ispremium: true,
+          })
+        );
+        alert("You are premium User Now");
+      },
+    };
+
+    var razorpayObject = new Razorpay(options);
+    razorpayObject.on("payment.failed", async function (response) {
+      await axios.post(
+        "http://localhost:3000/payment/updateOrder",
+        {
+          order_id: result.data.order.id,
+          payment_id: response.razorpay_payment_id,
+          status: "FAILED",
+        },
+        {
+          headers: { Authorization: token },
+        }
+      );
+      alert("This step of Payment Failed");
+    });
+    razorpayObject.open();
+  };
